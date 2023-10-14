4040583bbe594015d86d2652ecd4df741ec82fd4
Liubomyr Klymiuk <liubomyr.klymiuk@inveritasoft.com>
2023-10-14 21:54:36 +0300

    basic setup

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/public/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+        <div><h1>hello</h1></div>
+    </div>
+  )
+}
+
+export default App
